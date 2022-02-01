812736c92b4c90238a122d32f605d9694b33d4f9
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2022-01-31 19:24:06 -0800

    Fix esModule inconsistencies
    bump version

--- client/src/upload.ts
+++ client/src/upload.ts
@@ -1,4 +1,4 @@
-import { WebSocket, MessageEvent } from "isomorphic-ws"
+import WebSocket, { MessageEvent } from "isomorphic-ws"
 import { EventStateMatrix, Event, Action, State, BUFFER_SIZE } from "./state"
 
 type Signals =
